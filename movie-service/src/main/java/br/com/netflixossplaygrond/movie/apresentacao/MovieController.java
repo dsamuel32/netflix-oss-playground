a5dbaf29f81d424bd383e32139bdce8ccc132a61
dsamuel32 <diego.samuel.alves@gmail.com>
2018-06-11 20:34:34 -0300

    Adicionando movie service

---
+++ movie-service/src/main/java/br/com/netflixossplaygrond/movie/apresentacao/MovieController.java
@@ -0,0 +1,25 @@
+package br.com.netflixossplaygrond.movie.apresentacao;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+@RequestMapping(value = "movie", produces = MediaType.APPLICATION_JSON_VALUE)
+public class MovieController {
+
+    @GetMapping()
+    public ResponseEntity<Set<String>> getMovie() {
+        Set<String> movies = new HashSet<>();
+        movies.add("Vingadores Guerra Infinita");
+        movies.add("Spider-man");
+        return new ResponseEntity<>(movies, HttpStatus.OK);
+    }
+
+}
